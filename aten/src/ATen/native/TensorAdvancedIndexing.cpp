fc0242e11d78a98b454aaab2edf9181b9852c4dc
Joel Schlosser <jbschlosser@meta.com>
2026-05-13 12:55:18 -0400

    Remove named tensor
    ghstack-source-id: e93f9eec8b149a092881bc14a8f0a0dab5738eee
    Pull-Request: https://github.com/pytorch/pytorch/pull/173895

--- aten/src/ATen/native/TensorAdvancedIndexing.cpp
+++ aten/src/ATen/native/TensorAdvancedIndexing.cpp
@@ -58,7 +58,6 @@
 #include <ATen/Dispatch.h>
 #include <ATen/ExpandUtils.h>
 #include <ATen/MemoryOverlap.h>
-#include <ATen/NamedTensorUtils.h>
 #include <ATen/NumericUtils.h>
 #include <ATen/Parallel.h>
 #include <ATen/TensorIterator.h>
@@ -1895,8 +1894,6 @@ Tensor& index_fill_(
     int64_t dim,
     const Tensor& index,
     const Scalar& source) {
-  at::NoNamesGuard guard;
-
   TORCH_CHECK_INDEX(
       index.scalar_type() == ScalarType::Long,
       "index_fill_(): Expected dtype int64 for index.");
@@ -2433,7 +2430,6 @@ static Tensor& masked_fill_impl_cpu(
     Tensor& self,
     const Tensor& mask,
     const Scalar& value) {
-  NoNamesGuard guard;
   TORCH_CHECK(
       mask.dtype() == ScalarType::Bool,
       "masked_fill_ only supports boolean masks, but got mask "
@@ -2465,11 +2461,7 @@ Tensor& masked_fill__cpu(
     Tensor& self,
     const Tensor& mask,
     const Scalar& value) {
-  auto maybe_outnames =
-      namedinference::broadcast_to_outnames(self, mask, "masked_fill_");
-
   masked_fill_impl_cpu(self, mask, value);
-  namedinference::propagate_names_if_nonempty(self, maybe_outnames);
   return self;
 }
 
@@ -2477,8 +2469,6 @@ Tensor& masked_fill__cpu(
     Tensor& self,
     const Tensor& mask,
     const Tensor& value) {
-  auto maybe_outnames =
-      namedinference::broadcast_to_outnames(self, mask, "masked_fill_");
   TORCH_CHECK(
       value.dim() == 0,
       "masked_fill_ only supports a 0-dimensional value tensor, but got tensor "
@@ -2487,7 +2477,6 @@ Tensor& masked_fill__cpu(
       " dimension(s).");
 
   masked_fill_impl_cpu(self, mask, value.item());
-  namedinference::propagate_names_if_nonempty(self, maybe_outnames);
   return self;
 }
 
@@ -2496,15 +2485,9 @@ Tensor masked_fill(
     const Tensor& mask,
     const Scalar& source) {
   Tensor result;
-  auto maybe_outnames =
-      namedinference::broadcast_to_outnames(mask, self, "masked_fill");
-  {
-    NoNamesGuard guard;
-    auto [_mask, _self] = expand_outplace(mask, self);
-    result = _self->clone(at::MemoryFormat::Contiguous);
-    result.masked_fill_(mask, source);
-  }
-  namedinference::propagate_names_if_nonempty(result, maybe_outnames);
+  auto [_mask, _self] = expand_outplace(mask, self);
+  result = _self->clone(at::MemoryFormat::Contiguous);
+  result.masked_fill_(mask, source);
   return result;
 }
 
@@ -2513,15 +2496,9 @@ Tensor masked_fill(
     const Tensor& mask,
     const Tensor& source) {
   Tensor result;
-  auto maybe_outnames =
-      namedinference::broadcast_to_outnames(mask, self, "masked_fill");
-  {
-    NoNamesGuard guard;
-    auto [_mask, _self] = expand_outplace(mask, self);
-    result = _self->clone(at::MemoryFormat::Contiguous);
-    result.masked_fill_(mask, source);
-  }
-  namedinference::propagate_names_if_nonempty(result, maybe_outnames);
+  auto [_mask, _self] = expand_outplace(mask, self);
+  result = _self->clone(at::MemoryFormat::Contiguous);
+  result.masked_fill_(mask, source);
   return result;
 }
 
@@ -2529,8 +2506,6 @@ static Tensor& masked_select_out_impl_cpu(
     Tensor& result,
     const Tensor& self,
     const Tensor& mask) {
-  NoNamesGuard guard;
-
   TORCH_CHECK(
       mask.scalar_type() == ScalarType::Bool,
       "masked_select: expected BoolTensor for mask");
@@ -2614,7 +2589,6 @@ Tensor& masked_select_out_cpu(
     const Tensor& self,
     const Tensor& mask,
     Tensor& result) {
-  namedinference::compute_broadcast_outnames(self, mask);
   return masked_select_out_impl_cpu(result, self, mask);
 }
 
--- torchgen/model.py
+++ torchgen/model.py
@@ -2043,7 +2043,6 @@ class BaseTy(Enum):
     ScalarType = auto()
     Tensor = auto()
     int = auto()
-    Dimname = auto()
     DimVector = auto()
     float = auto()
     str = auto()
